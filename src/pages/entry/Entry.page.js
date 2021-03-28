bd07296829cd471f77c67b387cb7d6e9d39b031d
AmritSap <amritsapkota32@gmail.com>
2021-03-28 22:21:00 +1100

    Login page designed

---
+++ src/pages/entry/Entry.page.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Jumbotron } from "react-bootstrap";
+import { LoginForm } from "../../components/login/Login.comp";
+import "./entry.style.css";
+export const Entry = () => {
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+  };
+  return (
+    <div className="entry-page bg-info">
+      <Jumbotron className="form-box">
+        <LoginForm
+          handleOnChange={handleOnChange}
+          email={email}
+          pass={password}
+        />
+      </Jumbotron>
+    </div>
+  );
+};
