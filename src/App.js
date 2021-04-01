fb02be3d5d20cfd062ed1bca6b94c93232d5495a
AmritSap <amritsapkota32@gmail.com>
2021-04-01 16:19:44 +1100

    layout done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import React from 'react'
 import {Button} from 'react-bootstrap'
 import { Entry } from './pages/entry/Entry.page';
+import { DefaultLayout } from "./layout/DefaultLayout";
 
 function App() {
   return (
     <div className="App">
-      <Entry/>
+      {/* <Entry/> */}
+      <DefaultLayout>
+        main page component
+      </DefaultLayout>
     </div>
   );
 }
